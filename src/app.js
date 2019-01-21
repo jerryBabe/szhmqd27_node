f70baac65cdb0bc3c758c63099b743a6af54e2fb
jerryBabe <2451788514@qq.com>
2019-01-21 16:10:19 +0800

    完成了登录页面

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 //导包
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
+var session = require('express-session')
 
 //创建app
 const app = express()
 
-//导入路由对象
-const accountRouter = express(path.join(__dirname,'routers/accountRouter.js'))
+app.use(bodyParser.urlencode({extended:false})) 
+
+app.use(bodyParser.json())
+
+// Use the session middleware
+app.use(session({ secret: 'keyboard cat',resave:false, cookie: { maxAge: 600000 }}))
+
+//设置静态资源根目录
+app.use(express.static(path.join(__dirname,'public')))
+
+//导入路由对象,路由中间件写在最后 
+const accountRouter = require(path.join(__dirname,'routers/accountRouter.js'))
 app.use('/account',accountRouter)
 
 //启动
--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,4 +1,137 @@
+const path = require('path');
+const MongoClient = require("mongodb").MongoClient;
+const captchapng = require('captchapng')
 
+// Connection URL
+const url = "mongodb://locahost:27017";
+
+//Database Name 
+const dbName = "szhmqd27";
+
+//获取注册页面
 exports.getRegisterPage = (req,res) => {
     res.sendFile(path.join(__dirname,'../public/views/register.html'))
 }
+
+/**
+ * 导出的注册方法
+ */
+exports.register = (req,res) => {
+    const result = {
+        status: 0,
+        message: '注册成功'
+    }
+    //1.拿到浏览器传输过来的数据
+    const {username} = req.body
+
+    //2.先判断数据库中用户名是否存在，如果存在返回提示
+    MongoClient.connect(
+        url,
+        { useNewUrlParser: true },
+        function(err,client) {
+            //拿到db
+            const db = client.db(dbName);
+
+            //拿到集合
+            const collection = db.collection("accountInfo");
+
+            //查询一个
+            collection.findOne({ username }, (err,doc) => {
+                //如果result == null 没有查询到，就可以插入，如果查询到了说明用户已经存在
+                if (doc){
+                    //存在
+                    result.status = 1;
+                    result.message = "用户名已经存在"
+
+                    //关闭数据库
+                    client.close();
+
+                    //返回
+                    res.json(result);
+                }else {
+                    //如果用户名不存在，插入到数据库中
+                    //resul2有值，代表成功，result2为null就是失败
+                    collection.insertOne(req.body, (err,result2) => {
+                        if (!result2) {
+                            //失败
+                            result.status = 2;
+                            result.message = "注册失败";
+                        }
+
+                        //关闭数据库
+                        client.close();
+
+                        //返回
+                        res.json(result);
+
+                    })
+                }
+            })
+        }
+    )
+}
+
+//导出获取登录页面的方法
+exports.getLoginPage = (req,res ) =>{
+    res.sendFile(path.join(__dirname,"../public/views/login.html"))
+}
+
+exports.getVcodeImage = (req, res) => {
+    const vcode = parseInt(Math.random() * 9000 + 1000);
+    // 把vcode保存到session对象中去，方便将来登录
+    req.session.vcode = vcode
+    var p = new captchapng(80, 30, vcode); // width,height,numeric captcha
+    p.color(0, 0, 0, 0); // First color: background (red, green, blue, alpha)
+    p.color(80, 80, 80, 255); // Second color: paint (red, green, blue, alpha)
+  
+    var img = p.getBase64();
+    var imgbase64 = new Buffer(img, "base64");
+    res.writeHead(200, {
+      "Content-Type": "image/png"
+    });
+    res.end(imgbase64);
+  };
+
+  
+  //导出登录的方法
+  exports.login = (req,res) => {
+      const result = {
+          status: 0,
+          message: '登陆成功'
+      }
+
+      const { username,password,vcode} = req.body
+      
+      //验证验证码
+      if( vcode != req.session.vcode) {
+          result.status = 1,
+          result.message = '验证码错误'
+
+          res.json(result)
+          return
+      }
+
+      //验证码正确了
+      MongoClient.connect(
+          url,
+          { useNewUrlParser: true },
+          function (err,client) {
+              //拿到db对象
+              const db = client.db(dbName);
+
+              //拿到要操作的合集
+              const collection = db.collection("accountInfo");
+
+              //根据用户名或是密码查询
+              collection.findOne({ username,password }, (err,doc) => {
+                  if(!doc) {
+                      result.status = 2
+                      result.message = '用户名或是密码错误'
+                  }
+                  
+                  client.close();
+                  res.json(result)
+              })
+          }
+      )
+  }
